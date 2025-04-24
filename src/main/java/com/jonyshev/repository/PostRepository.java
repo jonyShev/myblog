e0bd3899c8ecbb9c2742f57f6c08c75713e9bb80
jonyShev <jony.a.shev@gmail.com>
2025-04-24 21:18:36 +0200

    Добавил PostRepository

---
+++ src/main/java/com/jonyshev/repository/PostRepository.java
@@ -0,0 +1,20 @@
+package com.jonyshev.repository;
+
+import com.jonyshev.model.Post;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PostRepository {
+    List<Post> findAll(String search, int pageSize, int pageNumber);
+
+    Optional<Post> findById(Long id);
+
+    Post save(Post post);
+
+    void update(Post post);
+
+    void deleteById(Long id);
+
+    void like(Long id, boolean increase);
+}
