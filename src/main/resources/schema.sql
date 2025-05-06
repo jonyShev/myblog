75310dfcb64642ae6508c858484b760d1ee26bb3
jonyShev <jony.a.shev@gmail.com>
2025-05-06 10:36:54 +0200

    Добавил IF NOT EXISTS при создании таблиц БД

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -1,13 +1,13 @@
-CREATE TABLE posts (
+CREATE TABLE IF NOT EXISTS posts (
     id BIGSERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     text TEXT,
     image_path VARCHAR(255),
     tags TEXT,
-    likes_count INTEGER DEFAULT = 0
+    likes_count INTEGER DEFAULT 0
 );
 
-CREATE TABLE comments (
+CREATE TABLE IF NOT EXISTS comments (
     id BIGSERIAL PRIMARY KEY,
     post_id BIGINT NOT NULL,
     text TEXT,
