bd89f2fe0a9ca6cf659ca5ed5d00c40f0a521297
jonyShev <jony.a.shev@gmail.com>
2025-05-06 10:36:15 +0200

    Добавил multipartResolver в WebMvcConfig, а также настрйоку в web.xml

--- src/main/java/com/jonyshev/config/WebMvcConfig.java
+++ src/main/java/com/jonyshev/config/WebMvcConfig.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
@@ -27,7 +29,7 @@ public class WebMvcConfig {
         templateResolver.setPrefix("/WEB-INF/templates/");
         templateResolver.setSuffix(".html");
         templateResolver.setCharacterEncoding("UTF-8");
-        templateResolver.setCacheable(false); // на время разработки
+        templateResolver.setCacheable(false);
         return templateResolver;
     }
 
@@ -45,5 +47,10 @@ public class WebMvcConfig {
         resolver.setTemplateEngine(templateEngine());
         resolver.setCharacterEncoding("UTF-8");
         return resolver;
+    }
+
+    @Bean
+    public MultipartResolver multipartResolver() {
+        return new StandardServletMultipartResolver();
     }
 }
