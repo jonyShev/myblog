894829317ffd4274f74c4e1a0293d4f7ac0ee0e3
jonyShev <jony.a.shev@gmail.com>
2025-05-07 11:35:14 +0200

    Добавил addResourceHandlers в конфигурацию для корректной работы с изображениями

--- src/main/java/com/jonyshev/config/WebMvcConfig.java
+++ src/main/java/com/jonyshev/config/WebMvcConfig.java
@@ -7,13 +7,15 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.multipart.MultipartResolver;
 import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring6.view.ThymeleafViewResolver;
 
 @Configuration
 @EnableWebMvc
-public class WebMvcConfig {
+public class WebMvcConfig implements WebMvcConfigurer {
 
     private final ApplicationContext applicationContext;
 
@@ -52,5 +54,11 @@ public class WebMvcConfig {
     @Bean
     public MultipartResolver multipartResolver() {
         return new StandardServletMultipartResolver();
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/uploads/**")
+                .addResourceLocations("file:" + System.getProperty("user.dir") + "/uploads/");
     }
 }
