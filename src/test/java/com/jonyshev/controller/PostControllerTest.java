677510b48cc2e5e97922ad9af77b32c1867f9cb5
jonyShev <jony.a.shev@gmail.com>
2025-05-22 10:35:27 +0200

    Создал класс PostControllerTest и метод getAllPosts. А также добавил необходимые зависимости в pom

---
+++ src/test/java/com/jonyshev/controller/PostControllerTest.java
@@ -0,0 +1,42 @@
+package com.jonyshev.controller;
+
+import com.jonyshev.config.AppConfig;
+import com.jonyshev.config.TestJdbcConfig;
+import com.jonyshev.config.WebMvcConfig;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(SpringExtension.class)
+@ContextConfiguration(classes = {AppConfig.class, WebMvcConfig.class, TestJdbcConfig.class})
+@WebAppConfiguration
+class PostControllerTest {
+
+    @Autowired
+    private WebApplicationContext wac;
+
+    private MockMvc mockMvc;
+
+    @BeforeEach
+    void setUp() {
+        mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+    }
+
+    @Test
+    void getAllPosts_shouldReturnPostsTemplate() throws Exception {
+        mockMvc.perform(get("/posts"))
+                .andExpect(status().isOk())
+                .andExpect(view().name("posts"))
+                .andExpect(model().attributeExists("posts", "search", "paging"));
+    }
+}
