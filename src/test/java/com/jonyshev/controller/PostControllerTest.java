1f2a2431f49887775ae57bb37bef9a4b6c6da0c0
jonyShev <jony.a.shev@gmail.com>
2025-05-22 11:12:17 +0200

    Добавил метод likePost в класс PostControllerTest

--- src/test/java/com/jonyshev/controller/PostControllerTest.java
+++ src/test/java/com/jonyshev/controller/PostControllerTest.java
@@ -3,18 +3,22 @@ package com.jonyshev.controller;
 import com.jonyshev.config.AppConfig;
 import com.jonyshev.config.TestJdbcConfig;
 import com.jonyshev.config.WebMvcConfig;
+import com.jonyshev.service.PostService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(SpringExtension.class)
@@ -27,6 +31,9 @@ class PostControllerTest {
 
     private MockMvc mockMvc;
 
+    @MockitoBean
+    private PostService postService;
+
     @BeforeEach
     void setUp() {
         mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
@@ -38,5 +45,16 @@ class PostControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(view().name("posts"))
                 .andExpect(model().attributeExists("posts", "search", "paging"));
+    }
+
+    @Test
+    void likePost_shouldRedirectAndCallService() throws Exception {
+        Long postId = 1L;
+
+        mockMvc.perform(post("/posts/1/like").param("like", "true"))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/posts/" + postId));
+
+        verify(postService).likePost(postId, true);
     }
 }
